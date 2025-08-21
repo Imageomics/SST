e9c200d823879d205d00f4c2544f29fe98afe20e
David Carlyn <davidecarlyn@gmail.com>
2025-08-21 10:02:50 -0400

    added cv2 package and fixed thresholding for mask

--- src/sst/get_mask_from_crop.py
+++ src/sst/get_mask_from_crop.py
@@ -13,31 +13,22 @@ args = parser.parse_args()
 
 img = cv2.imread(args.image_path, cv2.IMREAD_UNCHANGED)
 img_seg = cv2.imread(args.image_crop_path, cv2.IMREAD_UNCHANGED)
+img_seg[img_seg[:, :, 3] == 0] = [0, 0, 0, 0]
 
 w, h = img_seg.shape[1], img_seg.shape[0]
+res = cv2.matchTemplate(img, img_seg, method=cv2.TM_SQDIFF)
+loc = np.where(res == res.min())
 
-# Template matching
-best_value = None
-best_pos = None
-for x in range(img.shape[1]):
-    if x+w >= img.shape[1]:
-        break
-    for y in range(img.shape[0]):
-        if y+h >= img.shape[0]:
-            break
-        
-        diff = ((img[y:y+h, x:x+w, :3] - img_seg[:, :, :3])**2).sum()
-        if best_value is None or diff < best_value:
-            best_value = diff
-            best_pos = x, y 
+y, x = loc
+x = int(x)
+y = int(y)
             
-x, y = best_pos
 print(y, x)
 img = np.array(Image.open(args.image_path))
 img_seg = np.array(Image.open(args.image_crop_path))
 assert img_seg.shape[2] == 4, f"Image crop should have 4 channels (RGBA). Image has {img_seg.shape[2]} channels."
 
-mask = img_seg[:, :, 3] == 255
+mask = img_seg[:, :, 3] > 0
 
 mask_image = np.zeros(img.shape[:2], dtype=np.uint8)
 print(x, w, y, h)
